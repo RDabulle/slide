b94a9218c0158b42b6c2d9d151645fe9961794af
RDabulle <olives2739@gmail.com>
2023-08-30 03:07:44 +0900

    change the slide movement mechanism

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const leftButton = document.getElementById("left_button")
 const rightButton = document.getElementById("right_button")
 
-const slideContents = document.querySelectorAll(".slide_contents")
+const slideList = document.getElementsByClassName("slide_list")[0]
+
+const slideCount = slideList.childElementCount
 
 let i = 0
 
@@ -9,16 +11,12 @@ leftButton.addEventListener("click", () => {
     if (i > 0) {
         i = i - 1
     }
-    slideContents.forEach((element) => {
-        element.style.left = `${-100 * i}%`
-    })
+    slideList.style.left = `${-100 * i}%`
 })
 
 rightButton.addEventListener("click", () => {
-    if (i < slideContents.length - 1) {
+    if (i < slideCount - 1) {
         i = i + 1
     }
-    slideContents.forEach((element) => {
-        element.style.left = `${-100 * i}%`
-    })
+    slideList.style.left = `${-100 * i}%`
 })
